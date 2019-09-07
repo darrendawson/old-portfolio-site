52c2b0a94bbcf76f57ae7bdbfac7ee248ec54b72
Darren Dawson <dwdawson@ucsc.edu>
2019-09-06 20:53:54 -0700

    Add mobile layout to <TechChart/>

--- src/Components/TechChart/TechChart.js
+++ src/Components/TechChart/TechChart.js
@@ -12,15 +12,46 @@ class TechChart extends Component {
     super();
     this.state = {
       selectedSection: false,
-      selectedLanguage: false
+      selectedLanguage: false,
+      windowWidth: 0,
+      windowHeight: 0
     }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
+
+  // Window Orientation --------------------------------------------------------
+
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  // update Window sizes so <App/> will know whether it should render in mobile or landscape layout
+  updateWindowDimensions() {
+    this.setState({windowWidth: window.innerWidth, windowHeight: window.innerHeight});
+  }
+
+
+  // get -----------------------------------------------------------------------
+
+  // returns the selected section
+  // if in landscape mode, it will always return a section
+  // if in mobile, it can return false if no option is actually selected
   getSelectedSection = () => {
     if (this.state.selectedSection) {
       return this.state.selectedSection;
     } else {
-      return this.props.order[0];
+      if (this.state.windowWidth >= this.state.windowHeight) {
+        return this.props.order[0];
+      } else {
+        return false;
+      }
     }
   }
 
@@ -58,9 +89,35 @@ class TechChart extends Component {
     }
   }
 
-  // Render Math ---------------------------------------------------------------
+  // Render Landscape ----------------------------------------------------------
 
-  renderMathNode = (nodeID, nodeDetails) => {
+  renderLandscape = () => {
+
+    if (this.state.windowWidth < this.state.windowHeight) { return; }
+
+    return (
+      <div id="landscape_container">
+        <div id="card_container">
+          <div id="chart_title_row">
+            <h1 style={{'color': this.props.highlightColor, 'text-align': 'left', 'margin': '0px 40px 0px 0px', 'font-size': '3.5em'}}>Tech Stack</h1>
+          </div>
+
+          <div id="chart_title_row" style={{'padding-left': '55px', 'margin-top': '10px'}}>
+            {this.renderLanguagesUsed()}
+          </div>
+          {this.renderLandscape_TechItems()}
+        </div>
+
+        <div id="item_card_container">
+          {this.renderLandscape_SelectedCard()}
+        </div>
+
+      </div>
+    );
+  }
+
+
+  renderTechItem = (nodeID, nodeDetails) => {
 
     let nodeStyling = {'border': '4px solid transparent'};
     if (nodeID === this.getSelectedSection()) {
@@ -68,14 +125,14 @@ class TechChart extends Component {
     }
 
     return (
-      <div className="math_node_contanier" onClick={() => this.onClick_selectSection(nodeID)}>
+      <div className="tech_item" onClick={() => this.onClick_selectSection(nodeID)}>
         <img className="item_image" style={nodeStyling} src={nodeDetails.icon}/>
         <h3>{nodeDetails.title}</h3>
       </div>
     );
   }
 
-  renderMath = () => {
+  renderLandscape_TechItems = () => {
 
     // filter out items that weren't written with the selectedLanguage
     let order = this.getOrderForLanguage(this.state.selectedLanguage);
@@ -85,11 +142,11 @@ class TechChart extends Component {
     for (let i = 0; i < order.length; i++) {
       let nodeID = order[i];
       let nodeDetails = this.props.tech[nodeID];
-      mathToRender.push(this.renderMathNode(nodeID, nodeDetails));
+      mathToRender.push(this.renderTechItem(nodeID, nodeDetails));
 
       if (i + 1 < order.length) {
         mathToRender.push(
-          <div className="math_node_contanier">
+          <div className="tech_item">
             <h1>+</h1>
           </div>
         );
@@ -97,15 +154,15 @@ class TechChart extends Component {
     }
 
     return (
-      <div id="math_container">
+      <div id="tech_items_container">
         {mathToRender}
       </div>
     )
   }
 
-  // render card ---------------------------------------------------------------
 
-  renderSelectedCard = () => {
+  // render the card that shows details about the selected tech item
+  renderLandscape_SelectedCard = () => {
 
     let card = this.props.tech[this.getSelectedSection()];
     let footerText = "Written in ";
@@ -133,6 +190,127 @@ class TechChart extends Component {
     );
   }
 
+  // Render Mobile -------------------------------------------------------------
+
+  // when in mobile mode, it can render in two modes
+  // - if no item selected, show a list of all of the options
+  // - if an item is selected, show details about it
+  renderMobile = () => {
+    if (this.state.windowWidth >= this.state.windowHeight) { return; }
+
+    // option 1: no items are selected
+    if (this.getSelectedSection() === false) {
+      return (
+        <div id="mobile_container">
+          <h1 id="tech_stack_text" style={{'color': this.props.highlightColor, 'border-color': this.props.highlightColor}}>Tech Stack</h1>
+          <div id="languages_container">
+            {this.renderLanguagesUsed()}
+          </div>
+          {this.renderMobile_techItems()}
+          <p style={{'font-style': 'italic'}}>Click on an item to view it in more detail</p>
+        </div>
+      );
+
+    // option 2: an item is selected and we want to render details about it
+    } else {
+
+      let sectionDetails = this.props.tech[this.getSelectedSection()];
+      return (
+        <div id="mobile_container">
+          <div id="tech_stack_title_row">
+            <h1 style={{'color': this.props.highlightColor}}id="tech_item_title">{sectionDetails.title}</h1>
+            <button id="close_item_button" onClick={() => this.setState({'selectedSection': false, 'selectedLanguage': false})}>X</button>
+          </div>
+          {this.renderMobile_itemDescription()}
+          {this.renderMobile_itemLanguageText()}
+        </div>
+      );
+    }
+  }
+
+
+  renderMobile_techItems = () => {
+
+    // filter out items that weren't written with the selectedLanguage
+    let order = this.getOrderForLanguage(this.state.selectedLanguage);
+
+    let itemsToRender = [];
+    for (let i = 0; i < order.length; i++) {
+      let nodeID = order[i];
+      let nodeDetails = this.props.tech[nodeID];
+      itemsToRender.push(this.renderTechItem(nodeID, nodeDetails));
+    }
+
+    return (
+      <div id="tech_items_container">
+        {itemsToRender}
+      </div>
+    );
+  }
+
+
+  renderMobile_itemDescription = () => {
+
+    let selectedSection = this.getSelectedSection();
+    if (selectedSection === false) { return; }
+    let itemDetails = this.props.tech[selectedSection];
+    let description = itemDetails.description;
+
+    let textToRender = [];
+    for (let i = 0; i < description.length; i++) {
+
+      let textStyle = {};
+      if (description[i]['type'] === 'text') {
+        textStyle['color'] = this.props.highlightColor;
+        textStyle['font-size'] = '1.5em';
+        textStyle['margin'] = '10px 0px';
+      } else if (description[i]['type'] === 'list') {
+        textStyle['color'] = '#353535';
+        textStyle['margin'] = '5px';
+      }
+
+      textToRender.push(
+        <h3 style={textStyle}>{description[i]['text']}</h3>
+      );
+    }
+
+    return (
+      <div id="description_container">
+        {textToRender}
+      </div>
+    );
+  }
+
+  renderMobile_itemLanguageText = () => {
+
+    // get the current item's details
+    let selectedSection = this.getSelectedSection();
+    if (selectedSection === false) { return; }          // if there isn't a section, return
+    let itemDetails = this.props.tech[selectedSection];
+    let languages = itemDetails.languages;
+
+    // build the "written in" description String
+    let writtenInText = [];
+    writtenInText.push(<h3 className="written_in_text">Written in </h3>)
+    for (let i = 0; i < languages.length; i++) {
+      if ((i + 1 === languages.length) && (languages.length > 1)) {
+        writtenInText.push(<h3 className="written_in_text"> and </h3>);
+      }
+
+      if (i + 1 < languages.length) {
+        writtenInText.push(<h3  className="written_in_text" style={{'color': this.props.highlightColor}}>{languages[i]},</h3>);
+      } else {
+        writtenInText.push(<h3  className="written_in_text" style={{'color': this.props.highlightColor}}>{languages[i]}</h3>);
+      }
+    }
+
+    return (
+      <div id="written_in_text_container">
+        {writtenInText}
+      </div>
+    );
+  }
+
   // Renders <TechChart/> ------------------------------------------------------
 
   renderLanguagesUsed = () => {
@@ -156,24 +334,12 @@ class TechChart extends Component {
   }
 
 
+  // render <TechChart/>
   render() {
     return (
       <div id="TechChart">
-        <div id="card_container">
-          <div id="chart_title_row">
-            <h1 style={{'color': this.props.highlightColor, 'text-align': 'left', 'margin': '0px 40px 0px 0px', 'font-size': '3.5em'}}>Tech Stack</h1>
-          </div>
-
-          <div id="chart_title_row" style={{'padding-left': '55px', 'margin-top': '10px'}}>
-            {this.renderLanguagesUsed()}
-          </div>
-          {this.renderMath()}
-        </div>
-
-        <div id="item_card_container">
-          {this.renderSelectedCard()}
-        </div>
-
+        {this.renderLandscape()}
+        {this.renderMobile()}
       </div>
     );
   }
