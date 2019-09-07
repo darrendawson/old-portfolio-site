f8b5f21a7741afe25efcfde8ca6510a87bc995c6
Darren Dawson <dwdawson@ucsc.edu>
2019-09-06 23:15:17 -0700

    Add zoom in mode to <ImageSlideshow/>

--- src/Components/ImageSlideshow/ImageSlideshow.js
+++ src/Components/ImageSlideshow/ImageSlideshow.js
@@ -8,17 +8,23 @@ class ImageSlideshow extends Component {
 
     this.state = {
       imageIndex: 0,
-      seconds: 0
+      seconds: 0,
+      windowWidth: 0,
+      windowHeight: 0,
+      zoomInActive: false
     }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
-  // Clock ---------------------------------------------------------------------
+  // Clock and Window Size -----------------------------------------------------
   /*
     If untouched, pictures will scroll on a 5 second timer
   */
 
 
   componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
     this.timerID = setInterval(
       () => this.tick(),
       1000
@@ -26,15 +32,24 @@ class ImageSlideshow extends Component {
   }
 
   componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
     clearInterval(this.timerID);
   }
 
   tick() {
+
+    // if we are in zoom in mode in mobile, dont tick
+    if (this.state.zoomInActive && (this.state.windowWidth < this.state.windowHeight)) { return; }
+
     if (this.state.seconds > 5) {
       this.setState({'seconds': 0, 'imageIndex': this.getNextImageIndex()});
     } else {
       this.setState({'seconds': this.state.seconds + 1});
     }
+  }
+
+  updateWindowDimensions() {
+    this.setState({windowWidth: window.innerWidth, windowHeight: window.innerHeight});
   }
 
   // get -----------------------------------------------------------------------
@@ -66,12 +81,40 @@ class ImageSlideshow extends Component {
 
   // onClick -------------------------------------------------------------------
 
-  onClick_selectImage = (index) => {
+  // for when a user clicks on a different image in order to switch to it
+  onClick_selectImageFromGallery = (index) => {
     if (index < this.props.images.length) {
       this.setState({'imageIndex': index, 'seconds': 0});
     } else {
       this.setState({'imageIndex': 0, 'seconds': 0});
     }
+  }
+
+
+  // when a user clicks on an image with the intent to expand it
+  onClick_expandImage = (index = this.state.imageIndex) => {
+    if (this.state.windowWidth >= this.state.windowHeight) { return; }
+    this.setState({'zoomInActive': !this.state.zoomInActive});
+  }
+
+
+
+  // render Zoomed -------------------------------------------------------------
+
+  // only render zoomedIn mode if in mobile mode and zoomedIn is active
+  renderZoomed = () => {
+    if (this.state.windowWidth >= this.state.windowHeight) { return; }
+    if (!this.state.zoomInActive) { return; }
+
+    let image = this.getCurrentImage();
+
+    return (
+      <div id="zoomed_in_page_container" onClick={this.onClick_expandImage}>
+        <div id="zoomed_in_image_container">
+          <img id="zoomed_in_image" src={image.img}/>
+        </div>
+      </div>
+    );
   }
 
   // render --------------------------------------------------------------------
@@ -91,7 +134,7 @@ class ImageSlideshow extends Component {
       }
 
       buttons.push(
-        <button style={buttonStyle} className={buttonCSS} onClick={() => this.onClick_selectImage(i)}></button>
+        <button style={buttonStyle} className={buttonCSS} onClick={() => this.onClick_selectImageFromGallery(i)}></button>
       );
     }
 
@@ -110,14 +153,16 @@ class ImageSlideshow extends Component {
 
     return (
       <div id="ImageSlideshow">
-        <div id="image_container" style={{'backgroundImage': `url(${image.img})`}}>
+        <div id="image_container">
           <div id="title_container">
             <h1 id="img_title">{image.title}</h1>
             <h3 id="img_description">{image.description}</h3>
           </div>
+          <img id="image" src={image.img} onClick={this.onClick_expandImage}/>
         </div>
 
         {this.renderSelectImageButtons()}
+        {this.renderZoomed()}
       </div>
     );
   }
